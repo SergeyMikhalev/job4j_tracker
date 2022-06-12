35fccb3a3d124bb76aca1b4a4a58bd815342ce7b
Сергей <tolltroll@yandex.ru>
2022-06-12 17:30:08 +0300

    DB Storage

--- src/main/java/ru/job4j/tracker/store/StoreFactory.java
+++ src/main/java/ru/job4j/tracker/store/StoreFactory.java
@@ -4,8 +4,8 @@ public class StoreFactory {
     public static Store getStore(StoreTypeEnum type) {
         Store result = null;
         switch (type) {
-            case STORE_TYPE_MEM -> result = new MemTracker();
             case STORE_TYPE_SQL -> result = new SqlTracker();
+            default -> result = new MemTracker();
         }
         return result;
     }
