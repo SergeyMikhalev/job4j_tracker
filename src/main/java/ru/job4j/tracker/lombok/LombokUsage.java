ee5cb7de56a06ae1c3b9b739bb7177d1f1b53626
SergeyMikhalev <tolltroll@yandex.ru>
2022-11-22 17:17:01 +0300

    lombok builder

--- src/main/java/ru/job4j/tracker/lombok/LombokUsage.java
+++ src/main/java/ru/job4j/tracker/lombok/LombokUsage.java
@@ -5,7 +5,25 @@ public class LombokUsage {
         var bird = new BirdData();
         bird.setAge(1);
         System.out.println(bird);
+
         var category = new Category(1);
         System.out.println(category);
+
+        var role = Role.of()
+                .id(1)
+                .name("ADMIN")
+                .accessBy("create")
+                .accessBy("update")
+                .accessBy("read")
+                .accessBy("delete")
+                .build();
+        System.out.println(role);
+
+        var permission = Permission.of()
+                .id(34).name("TOTAL")
+                .addRule("rule1")
+                .addRule("rule34")
+                .build();
+        System.out.println(permission);
     }
 }
