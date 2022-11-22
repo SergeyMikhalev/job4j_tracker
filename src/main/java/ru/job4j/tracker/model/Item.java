470873025dad4dae1f6537ff6095019f06a4a6d1
SergeyMikhalev <tolltroll@yandex.ru>
2022-11-22 16:39:04 +0300

    Lombok first try

--- src/main/java/ru/job4j/tracker/model/Item.java
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Objects;
 
 @Data
@@ -13,9 +15,15 @@ public class Item {
 
     private int id;
     private String name;
-    private LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
 
     public Item(String name) {
         this.name = name;
+    }
+
+    /* При сохранении в базу происходит округление времени,
+     поэтому для прохождения тестов округляю время создания */
+    public void setCreated(LocalDateTime created) {
+        this.created = created.truncatedTo(ChronoUnit.MILLIS);
     }
 }
