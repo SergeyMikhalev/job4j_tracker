d417c5621dd2f361c030ac799b93af8982356370
SergeyMikhalev <tolltroll@yandex.ru>
2022-11-22 18:57:21 +0300

    HQLUsage

---
+++ src/main/java/ru/job4j/tracker/HQLUsage.java
@@ -0,0 +1,79 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+import ru.job4j.tracker.model.Item;
+
+public class HQLUsage {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try (SessionFactory sf = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory()) {
+            Session session = sf.openSession();
+            insert(session);
+            session.close();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    public static void select(Session session) {
+        Query query = session.createQuery("from Item as i where i.name = 'Item 1'");
+        for (Object st : query.list()) {
+            System.out.println(st);
+        }
+    }
+
+    public static void findById(Session session, int id) {
+        Query<Item> query = session.createQuery(
+                "from Item as i where i.id = :fId", Item.class);
+        query.setParameter("fId", id);
+        System.out.println(query.uniqueResult());
+    }
+
+    public static void update(Session session, int id) {
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "UPDATE Item SET name = :fName WHERE id = :fId")
+                    .setParameter("fName", "new name")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    public static void delete(Session session, int id) {
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE Item WHERE id = :fId")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    public static void insert(Session session) {
+        try {
+            session.beginTransaction();
+            session.createNativeQuery(
+                            "INSERT INTO items (name) VALUES (:fName)")
+                    .setParameter("fName", "new name")
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+}
+
