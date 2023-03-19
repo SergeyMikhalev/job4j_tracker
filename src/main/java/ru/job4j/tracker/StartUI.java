8f9e80aaf582b58c0b477e302216dd5c76be3f82
Сергей <tolltroll@yandex.ru>
2023-03-19 18:00:36 +0300

    Версия для развертывания в контейнере Docker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,6 @@ import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.input.ValidateInput;
 import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
-import ru.job4j.tracker.store.HbmTracker;
 import ru.job4j.tracker.store.MemTracker;
 import ru.job4j.tracker.store.Store;
 
